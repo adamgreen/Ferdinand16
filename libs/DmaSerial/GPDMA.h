f200ef2a5673c6b797c5906f9cb5fe44ab786e50
Adam Green <adamgr@foo.bar>
2016-07-19 13:25:41 -0700

    Build DmaSerial as a library
    I want to share the DmaSerial functionality between the basic heading
    firmware that I have been working on so far and the other firmware
    variants that I plan to create in the future. To facilitate this, I
    have moved the DMA code out into its own libs/DmaSerial directory. I
    also replaced DMA.h with GPDMA.h/c from my NeoPixelTree project where
    I added GPDMA channel allocation functionality. This will allow me to
    have more than a single DmaSerial object instantiated (one for logging
    and the other for the Parallax DHB-10 Dual Motor Controller.)

--- libs/DmaSerial/GPDMA.h
+++ libs/DmaSerial/GPDMA.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2014  Adam Green (https://github.com/adamgreen)
+/*  Copyright (C) 2016  Adam Green (https://github.com/adamgreen)
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
@@ -41,7 +41,6 @@
 #define DMACCxCONFIG_ITC                    (1 << 15)
 #define DMACCxCONFIG_ACTIVE                 (1 << 17)
 #define DMACCxCONFIG_HALT                   (1 << 18)
-
 #define DMA_PERIPHERAL_SSP0_TX              0
 #define DMA_PERIPHERAL_SSP0_RX              1
 #define DMA_PERIPHERAL_SSP1_TX              2
@@ -58,11 +57,48 @@
 #define DMA_PERIPHERAL_UART2RX_MAT2_1       13
 #define DMA_PERIPHERAL_UART3TX_MAT3_0       14
 #define DMA_PERIPHERAL_UART3RX_MAT3_1       15
-
 #define DMACCxCONFIG_TRANSFER_TYPE_M2M      (0 << DMACCxCONFIG_TRANSFER_TYPE_SHIFT)
 #define DMACCxCONFIG_TRANSFER_TYPE_M2P      (1 << DMACCxCONFIG_TRANSFER_TYPE_SHIFT)
 #define DMACCxCONFIG_TRANSFER_TYPE_P2M      (2 << DMACCxCONFIG_TRANSFER_TYPE_SHIFT)
 #define DMACCxCONFIG_TRANSFER_TYPE_P2P      (3 << DMACCxCONFIG_TRANSFER_TYPE_SHIFT)
+
+typedef struct
+{
+    __IO uint32_t   DMACCxSrcAddr;
+    __IO uint32_t   DMACCxDestAddr;
+    __IO uint32_t   DMACCxLLI;
+    __IO uint32_t   DMACCxControl;
+} DmaLinkedListItem;
+
+typedef enum
+{
+    GPDMA_CHANNEL0 = 0,
+    GPDMA_CHANNEL1 = 1,
+    GPDMA_CHANNEL2 = 2,
+    GPDMA_CHANNEL3 = 3,
+    GPDMA_CHANNEL4 = 4,
+    GPDMA_CHANNEL5 = 5,
+    GPDMA_CHANNEL6 = 6,
+    GPDMA_CHANNEL7 = 7,
+    GPDMA_CHANNEL_LOWEST = GPDMA_CHANNEL7,
+    GPDMA_CHANNEL_HIGHEST = GPDMA_CHANNEL0,
+    GPDMA_CHANNEL_MEM2MEM = GPDMA_CHANNEL_LOWEST,
+    GPDMA_CHANNEL_HIGH = -1,                        // Search from 0 up until find unused channel.
+    GPDMA_CHANNEL_LOW = 0x7FFFFFFF                  // Search from 6 down until find unused channel.
+} DmaDesiredChannel;
+
+typedef struct DmaInterruptHandler
+{
+    uint32_t (*handler)(void* pContext, uint32_t dmaInterruptStatus);
+    void*                       pContext;
+    struct DmaInterruptHandler* pNext;
+} DmaInterruptHandler;
+
+typedef struct DmaMemCopyCallback
+{
+    void (*handler)(void* pContext);
+    void* pContext;
+} DmaMemCopyCallback;
 
 
 static __INLINE void enableGpdmaPower(void)
@@ -74,5 +110,33 @@ static __INLINE void enableGpdmaInLittleEndianMode(void)
 {
     LPC_GPDMA->DMACConfig = 1;
 }
+
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+int                  allocateDmaChannel(DmaDesiredChannel desiredChannel);
+void                 freeDmaChannel(int channel);
+LPC_GPDMACH_TypeDef* dmaChannelFromIndex(int index);
+
+int                  addDmaInterruptHandler(DmaInterruptHandler* pHandler);
+int                  removeDmaInterruptHandler(DmaInterruptHandler* pHandler);
+
+int                  dmaMemCopy(void* pDest, const void* pSrc, size_t size, const DmaMemCopyCallback *pCallback);
+void                 uninitDmaMemCopy(void);
+
+// Allocated memory from AHBSRAM0 and AHBSRAM1 banks meant for DMA usage.
+// These allocations are byte aligned and can't be freed. They also don't check for out of memory.
+void*                dmaHeap0Alloc(uint32_t size);
+void*                dmaHeap1Alloc(uint32_t size);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* DMA_H_ */
